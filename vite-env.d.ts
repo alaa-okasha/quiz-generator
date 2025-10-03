6f5089ecb49614a018837a0c9fc0d06bc0d6455b
Alaa <aayyaser@hotmail.com>
2025-10-03 17:37:15 +0300

    upload files

---
+++ vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  // add more env vars here if you need them
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
